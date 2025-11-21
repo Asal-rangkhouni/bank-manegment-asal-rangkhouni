00e95c0419053b4a5e8604cc17ceba80ebc1a38c
Ali Pilehvar Meibody <ali.pilehvarmeibody@studenti.polito.it>
2025-11-21 22:07:30 +0100

    Clean up blank lines in utils.py
    Removed unnecessary blank lines in utils.py.

--- pppp/utils.py
+++ pppp/utils.py
@@ -4,8 +4,6 @@ def hash_password(password):
     hashed_password=hashlib.sha256(password.encode()).hexdigest()
     return hashed_password
 
-
-
 def check_password(hashed_password,plain_password):
     if hashed_password==hash_password(plain_password):
         return True
