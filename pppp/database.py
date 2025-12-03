e5e0c19d4048b3bf57e572d05bccb71154ed0856
Ali Pilehvar Meibody <ali.pilehvarmeibody@studenti.polito.it>
2025-12-03 17:46:13 +0100

    Update database.py

--- pppp/database.py
+++ pppp/database.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base,sessionmaker
 
-#mysql , ....
 DATABASE_URL = "sqlite:///database.db"
 
 engine= create_engine("mysql+mysqlconnector://asal:Milad004800@localhost",echo=False, future=True)
@@ -10,8 +9,6 @@ Base = declarative_base()
 
 SessionLocal = sessionmaker(bind=engine , autoflash=False, autocommit=False,future=True )
 
-
 #helper
-
 def get_session():
     return SessionLocal()
